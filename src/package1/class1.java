fc2a892fe55b810dea044c2da16fcec4e7a880ac
ikriuchk <iuliia.kriuchkova@t-systems.com>
2021-03-15 18:03:05 +0300

    commit 2

--- src/package1/class1.java
+++ src/package1/class1.java
@@ -2,10 +2,16 @@ package package1;
 
 public class class1 {
     public static void main(String[] args) {
+        //ЦИКЛ WHILE
         int i = 0;
         while (i < 10) {
             i++;
             System.out.println("Hello Julia" + i);
+        }
+
+        //цикл for
+        for (int n = 0;n<10;n++){
+            System.out.println("Hey you" +  n);
         }
     }
 }
