6a6c18e8bec8840947a36f662ee693a5665115ad
ikriuchk <iuliia.kriuchkova@t-systems.com>
2021-03-21 16:27:42 +0300

    commit with Hello world

--- src/Labs/lab1_HelloWorld.java
+++ src/Labs/lab1_HelloWorld.java
@@ -1,15 +1,17 @@
-package package1;
+package Labs;
 
-public class class1 {
+public class lab1_HelloWorld {
     public static void main(String[] args) {
         //ЦИКЛ WHILE
+        System.out.println("Here is while!");
         int i = 0;
         while (i < 10) {
             i++;
-            System.out.println("Hello Word" + i);
+            System.out.println("Hello Word"+ " "  + i);
         }
 
         //цикл for
+        System.out.println("Here is for!");
         for (int n = 0;n<100; n++){
             System.out.println("Hey you"+" " +  n);
         }
