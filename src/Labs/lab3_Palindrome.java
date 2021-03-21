6037142b444b0d9a1292480c4892d4829ed12881
ikriuchk <iuliia.kriuchkova@t-systems.com>
2021-03-21 23:41:25 +0300

    commit with lab3

--- src/Labs/lab3_Palindrome.java
+++ src/Labs/lab3_Palindrome.java
@@ -6,11 +6,17 @@ public class lab3_Palindrome {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Type the number");
-        int num=in.nextInt();
-        if (num >=9999 && num/10000 == num % 10 && (num/1000) % 10 == (num % 100)/10){
-        System.out.println("This is a palindrome number");}
-        else System.out.println("This isn't a palindrome number with 5 characters");
+        int num = in.nextInt();
+        if (num / 10000 == num % 10 && num > 9999 && num < 100000) {
+            System.out.println("This is a palindrome number with 5 characters");
+        } else if (num < 9999) {
+            System.out.println("Type in the number with 5 characters");
+        } else System.out.println("This isn't a palindrome number");
+
     }
 }
 
+//if (num/10000 == num % 10 && (num/1000) % 10 == (num % 100)/10)
 //54045
+//939
+//54035
