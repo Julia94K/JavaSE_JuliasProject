2839636750f83fc5af3e5126d97e8f6f9370f20c
ikriuchk <iuliia.kriuchkova@t-systems.com>
2021-04-04 16:43:48 +0300

    алфавит

---
+++ src/Labs/lab4_alphabet.java
@@ -0,0 +1,9 @@
+package Labs;
+
+public class lab4_alphabet {
+    public static void main(String[] args) {
+        for (char i = 'A' ; i <= 'Z' ; i++) {
+            System.out.println(i);
+        }
+    }
+}
