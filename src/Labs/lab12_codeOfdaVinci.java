021272fa3a472fcf4d9804dc496ce75c6c5faf93
Julia94K <kryuchkova.julia@yandex.ru>
2021-04-11 01:25:22 +0300

    commit Fibonachi add

--- src/Labs/lab12_codeOfdaVinci.java
+++ src/Labs/lab12_codeOfdaVinci.java
@@ -4,17 +4,26 @@ import java.util.Scanner;
 
 public class lab12_codeOfdaVinci {
     public static void main(String[] args) {
+        System.out.println("Введите количество чисел фибоначи: ");
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int l = 1;
         int r = 1;
         System.out.println(l);
-        int nextNumber = 0;
-        while (nextNumber <= N) {
+        int nextNumber;
+        int count =0;
+        while (count < N-1){
             nextNumber = l + r;
             l = r;
             r = nextNumber;
             System.out.println(nextNumber);
+            count++;
         }
+//        while (nextNumber <= N) {
+//            nextNumber = l + r;
+//            l = r;
+//            r = nextNumber;
+//            System.out.println(nextNumber);
+//        }
     }
 }
