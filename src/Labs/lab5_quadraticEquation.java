cbf4985a56a2a271d288128d3825a818f3fe1d81
Julia94K <kryuchkova.julia@yandex.ru>
2021-04-11 20:53:43 +0300

    commit quadratic equation add

--- src/Labs/lab5_quadraticEquation.java
+++ src/Labs/lab5_quadraticEquation.java
@@ -4,21 +4,33 @@ package Labs;
 //Использовать класс Scanner для ввода значений с клавиатуры."
 
 import java.util.Scanner;
-
+//например, a = 1; b = −6; c = 9 ->  1 решение
+//a = 1, b = −8, c = 12; -> 2 решения
+//a = 5; b = 3; c = 7; - > нет решения
 public class lab5_quadraticEquation {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите число x");
-        int x = in.nextInt();
         System.out.println("Введите число a");
         int a = in.nextInt();
         System.out.println("Введите число b");
         int b = in.nextInt();
         System.out.println("Введите число c");
         int c = in.nextInt();
-        int num;
-        num = (a*x*x)+(b*x)+c;
-        System.out.println("ax^2 + bx + c = "+ num);
+        int x;
+        int D = (b * b) - (4 * a * c);
+        if (D < 0) {
+            System.out.println("Решение отсутствует");
+        } else if (D == 0) {
+            x = -b / (2 * a);
+            System.out.println("x = " + x);
+        } else {
+            double x1;
+            x1 = (-b + Math.sqrt(D)) /(2 * a);
+            double x2;
+            x2 = (-b - Math.sqrt(D)) /(2 * a);
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        }
     }
 }
 
