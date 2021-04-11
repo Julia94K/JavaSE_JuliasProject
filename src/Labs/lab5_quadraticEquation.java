e7c03f8a2bc967a657e55e00d175d3adedf463a3
Julia94K <kryuchkova.julia@yandex.ru>
2021-04-11 21:36:55 +0300

    commit quadratic equation addition changes

--- src/Labs/lab5_quadraticEquation.java
+++ src/Labs/lab5_quadraticEquation.java
@@ -28,8 +28,8 @@ public class lab5_quadraticEquation {
             x1 = (-b + Math.sqrt(D)) /(2 * a);
             double x2;
             x2 = (-b - Math.sqrt(D)) /(2 * a);
-            System.out.println("x1 = " + x1);
-            System.out.println("x2 = " + x2);
+            System.out.println("x1 = " + ((int) x1));
+            System.out.println("x2 = " + ((int)x2));
         }
     }
 }
