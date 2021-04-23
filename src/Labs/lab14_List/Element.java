13afebd7f374f8a4c92d6b21d49818ba5fefbb2a
Julia94K <kryuchkova.julia@yandex.ru>
2021-04-23 23:09:55 +0300

    commit 14

---
+++ src/Labs/lab14_List/Element.java
@@ -0,0 +1,13 @@
+package Labs.lab14_List;
+
+public class Element {
+    int value;
+
+    Element element; // ссылка на самого себя
+
+    public Element(int v) {
+        this.value = v;
+        //this.count = c;
+
+    }
+}
---
+++ src/Labs/lab14_List/MyLovelyList.java
@@ -0,0 +1,60 @@
+package Labs.lab14_List;
+//Односвязный список
+//        1) Реализовать класс Element && класс List для хранения данных типа int.
+//        2) в классе List реализовать метод addElement() - добавление элемента в начало списка.
+//        3) в классе List реализовать метод getSize () - возвращает размер списка. Нужно сделать так,
+//        чтобы при вызове этого метода он возвращал одно закешированное значение.
+//        Не нужно чтобы метод проходил по всему списку.
+//        4) в классе List реализовать метод remove() - удаление первого элемента списка и возвращение
+//        этого значения в качестве результата работы метода.
+//        Проверить работу этого метода в совокупности с методом add()
+//        Запрещено использовать массивы и другие коллекции объектов.
+public class MyLovelyList {
+    Element head = null;
+
+
+    public int getListSize(){
+        int count = 0;
+        Element t = head;
+        while (t!= null){
+            t = t.element;
+            count ++;
+        }
+        return count;
+    }
+    public void addToList (Element newElement){
+        newElement.element=head;
+        head=newElement;
+    }
+    public void showList (){
+        Element t = head;
+        while (t != null){
+            System.out.println(t.value);
+            t=t.element;
+        }
+    }
+    public void remove(){
+        Element firstElement = head;
+        head = firstElement.element;
+//        Element secondElement= firstElement.element;
+//        head = secondElement;
+        //(int)secondElement = newFirst;
+        System.out.println("Мы удалили "+firstElement.value);
+
+
+    }
+
+    public static void main(String[] args) {
+        MyLovelyList myList = new MyLovelyList();
+        myList.addToList(new Element(101));//сначала добавляем 3й элемент
+        myList.addToList(new Element(202));//теперь добавляем 2й элемент
+        myList.addToList(new Element(103));//теперь 1й
+        myList.showList();
+        System.out.println(myList.getListSize());
+        myList.remove();
+        System.out.println("Остались следующие элементы:");
+        myList.showList();
+
+    }
+
+}
